820f33fe0d5da2bf9f90516f94e34158ee7000ad
Alaa Bashir <abashir@lab1r4s1.42abudhabi.ae>
2024-03-28 16:32:46 +0400

    add comments

--- inception/srcs/requirements/mariadb/tools/setup_db.sh
+++ inception/srcs/requirements/mariadb/tools/setup_db.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+# Create MySQL data directory for MariaDB runtime data and db
 mkdir -p /run/mysqld && mkdir -p /var/lib/mysql
+
+
+# Change the ownership of the MySQL data directory to the MySQL server
 chown -R mysql:mysql /run/mysqld /var/lib/mysql
 
-
+# Set up the initial data structure and tables required for MySQL
 if [ ! -d /var/lib/mysql/mysql ]; then
     if mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null; then
 		echo "[i] MySQL database successfully installed."
@@ -12,6 +16,7 @@ if [ ! -d /var/lib/mysql/mysql ]; then
     fi
 fi
 
+# MYSQL Configuration file
 cat << EOF > tmpfile
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
@@ -21,15 +26,9 @@ GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 
+# Set up initial configuration for MySQL
 /usr/bin/mysqld --user=mysql --bootstrap --skip-name-resolve --skip-networking=0 < tmpfile
 
 rm -f tmpfile
 
 exec /usr/bin/mysqld --user=mysql --skip-name-resolve --skip-networking=0 $@
-
-# connect to MySQL as the root user: mysql -u root -p (then enter the root password).
-# Show the list of databases: SHOW DATABASES;
-# Use the 'wordpress' database: use 'wordpress';
-# Show the tables in the selected database: SHOW TABLES;
-# Select the display name of users from the 'wp_users' table: SELECT wp_users.display_name FROM wp_users;
-# Select all columns from the 'wp_users' table: SELECT * FROM wp_users;
